93d0c10d830648f6eac14a4f2d901fea01b03e00
ppannuto <ppannuto@40ecb139-3c61-4c5f-bdaf-cae60a0fc81e>
2011-09-22 05:39:07 +0000

    Add printf stubs
    git-svn-id: svn+ssh://energy.eecs.umich.edu/svnroot/teaching/eecs373-f11/tools/sim@36 40ecb139-3c61-4c5f-bdaf-cae60a0fc81e

---
+++ programs/lib/printf.c
@@ -0,0 +1,8 @@
+#include "printf.h"
+
+/* Suggest getting it working for a fixed number
+   of arguments first. Also, you do not have to
+   support every format specifier that printf does,
+   the "major" ones are sufficient: d, u, x, c, s */
+
+int printf(const char *format, ...);
---
+++ programs/lib/printf.h
@@ -0,0 +1,6 @@
+#ifndef __PRINTF_H
+#define __PRINTF_H
+
+int printf(const char *format, ...);
+
+#endif //__PRINTF_H
