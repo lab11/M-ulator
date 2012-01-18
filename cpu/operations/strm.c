fbcdb9211cbdcf3a5b38721df4eb8f7391d08878
ppannuto <ppannuto@40ecb139-3c61-4c5f-bdaf-cae60a0fc81e>
2012-01-18 23:44:06 +0000

    Add strmia
    git-svn-id: svn+ssh://energy.eecs.umich.edu/svnroot/teaching/eecs373-f11/tools/sim@186 40ecb139-3c61-4c5f-bdaf-cae60a0fc81e

--- cpu/operations/strm.c
+++ cpu/operations/strm.c
@@ -39,8 +39,50 @@ static void stmdb_t1(uint32_t inst) {
 	stmdb(rn, registers, W);
 }
 
+void stm(uint8_t rn, uint16_t registers, bool wback) {
+	uint32_t rn_val = CORE_reg_read(rn);
+
+	uint32_t address = rn_val;
+
+	int i;
+	for (i = 0; i <= 14; i++) {
+		if (registers & (1 << i)) {
+			if ((i == rn) && wback) {
+				CORE_ERR_not_implemented("complicated case\n");
+			} else {
+				write_word(address, CORE_reg_read(i));
+			}
+			address += 4;
+		}
+	}
+
+	if (wback)
+		CORE_reg_write(rn, rn_val + 4*hamming(registers));
+}
+
+void stm_t2(uint32_t inst) {
+	uint16_t register_list = inst & 0xfff;
+	bool M = !!(inst & 0x4000);
+	uint8_t rn = (inst >> 16) & 0xf;
+	bool W = !!(inst & 0x200000);
+
+	uint16_t registers = (M << 13) | register_list;
+	bool wback = W;
+
+	if ((rn == 15) || (hamming(registers) < 2))
+		CORE_ERR_unpredictable("bad regs\n");
+
+	if (wback && (registers & (1 << rn)))
+		CORE_ERR_unpredictable("wback conflict\n");
+
+	return stm(rn, registers, wback);
+}
+
 void register_opcodes_strm(void) {
 	// 1110 1001 00x0 xxxx 0x0x xxxx xxxx xxxx
 	//             1  1101
 	register_opcode_mask_ex(0xe9000000, 0x16d0a000, stmdb_t1, 0x002d0000, 0x00020000, 0, 0);
+
+	// stm{ia,ea}_t2: 1110 1000 10x0 xxxx 0x0x xxxx xxxx xxxx
+	register_opcode_mask(0xe8800000, 0x1750a000, stm_t2);
 }
