f880245137915da481cd707afd1aabecd31b4fd3
Pat Pannuto <pat.pannuto@gmail.com>
2013-07-11 16:59:57 -0700

    Add some missing logicals; some annotations

--- simulator/core/operations/logical.c
+++ simulator/core/operations/logical.c
@@ -23,25 +23,6 @@
 #include "cpu/registers.h"
 #include "cpu/misc.h"
 
-static void and(uint16_t inst) {
-	uint8_t rm = (inst & 0x38) >> 3;
-	uint8_t rd = (inst & 0x7) >> 0;
-
-	uint32_t result;
-	result = CORE_reg_read(rd) & CORE_reg_read(rm);
-	CORE_reg_write(rd, result);
-
-	union apsr_t apsr = CORE_apsr_read();
-
-	if (!in_ITblock()) {
-		apsr.bits.N = HIGH_BIT(result);
-		apsr.bits.Z = result == 0;
-		CORE_apsr_write(apsr);
-	}
-
-	DBG2("ands r%02d, r%02d\n", rd, rm);
-}
-
 static void and_imm(union apsr_t apsr, uint8_t setflags, uint8_t rd, uint8_t rn,
 		uint32_t imm32, uint8_t carry) {
 	uint32_t rn_val = CORE_reg_read(rn);
@@ -63,6 +44,7 @@ static void and_imm(union apsr_t apsr, uint8_t setflags, uint8_t rd, uint8_t rn,
 	DBG2("and_imm done\n");
 }
 
+// arm-v7-m
 static void and_imm_t1(uint32_t inst) {
 	union apsr_t apsr = CORE_apsr_read();
 
@@ -113,6 +95,21 @@ static void and_reg(uint8_t rd, uint8_t rn, uint8_t rm,
 	}
 }
 
+// arm-thumb
+static void and_reg_t1(uint16_t inst) {
+	uint8_t rdn = inst & 0x7;
+	uint8_t rm  = (inst >> 3) & 0x7;
+
+	uint8_t rd = rdn;
+	uint8_t rn = rdn;
+	bool setflags = !in_ITblock();
+	enum SRType shift_t = SRType_LSL;
+	uint8_t shift_n = 0;
+
+	return and_reg(rd, rn, rm, setflags, shift_t, shift_n);
+}
+
+// arm-v7-m
 static void and_reg_t2(uint32_t inst) {
 	uint8_t rm = inst & 0xf;
 	uint8_t type = (inst >> 4) & 0x3;
@@ -133,6 +130,61 @@ static void and_reg_t2(uint32_t inst) {
 		CORE_ERR_unpredictable("bad reg\n");
 
 	return and_reg(rd, rn, rm, setflags, shift_t, shift_n);
+}
+
+static inline void bfc(uint8_t rd, uint8_t msbit, uint8_t lsbit) {
+	if (lsbit < msbit)
+		CORE_ERR_unpredictable("bfc msbit < lsbit?\n");
+
+	uint32_t rd_val = CORE_reg_read(rd);
+	uint32_t mask = ((1 << (msbit+1)) - 1) - ((1 << lsbit) - 1);
+	rd_val &= ~mask;
+	CORE_reg_write(rd, rd_val);
+}
+
+// arm-v7-m
+static void bfc_t1(uint32_t inst) {
+	uint8_t msb  = inst & 0x1f;
+	uint8_t imm2 = (inst >> 6) & 0x3;
+	uint8_t rd   = (inst >> 8) & 0xf;
+	uint8_t imm3 = (inst >> 12) & 0x7;
+
+	uint8_t msbit = msb;
+	uint8_t lsbit = imm2 | (imm3 << 2);
+
+	if (rd > 13)
+		CORE_ERR_unpredictable("bfc_t1 case\n");
+
+	return bfc(rd, msbit, lsbit);
+}
+
+static inline void bfi(uint8_t rd, uint8_t rn, uint8_t msbit, uint8_t lsbit) {
+	if (lsbit < msbit)
+		CORE_ERR_unpredictable("bfi msbit < lsbit?\n");
+
+	uint32_t rd_val = CORE_reg_read(rd);
+	uint32_t rn_val = CORE_reg_read(rn);
+	uint32_t mask = ((1 << (msbit+1)) - 1) - ((1 << lsbit) - 1);
+	rd_val &= ~mask;
+	rd_val |= (rn_val & (mask >> lsbit)) << lsbit;
+	CORE_reg_write(rd, rd_val);
+}
+
+// arm-v7-m
+static void bfi_t1(uint32_t inst) {
+	uint8_t msb  = inst & 0x1f;
+	uint8_t imm2 = (inst >> 6) & 0x3;
+	uint8_t rd   = (inst >> 8) & 0xf;
+	uint8_t imm3 = (inst >> 12) & 0x7;
+	uint8_t rn   = (inst >> 16) & 0xf;
+
+	uint8_t msbit = msb;
+	uint8_t lsbit = imm2 | (imm3 << 2);
+
+	if ((rd > 13) || (rn == 13))
+		CORE_ERR_unpredictable("bfi_t1 case\n");
+
+	return bfi(rd, rn, msbit, lsbit);
 }
 
 static void bic(uint16_t inst) {
@@ -453,17 +505,25 @@ static void tst_imm_t1(uint32_t inst) {
 
 __attribute__ ((constructor))
 void register_opcodes_logical(void) {
-	// and: 0100 0000 00<x's>
-	register_opcode_mask_16(0x4000, 0xbfc0, and);
-
 	// and_imm_t1: 1111 0x00 000x xxxx 0<x's>
 	register_opcode_mask_32_ex(0xf0000000, 0x0be08000, and_imm_t1,
 			0x00100f00, 0x0,
 			0, 0);
 
+	// and_reg_t1: 0100 0000 00xx xxxx
+	register_opcode_mask_16(0x4000, 0xbfc0, and_reg_t1);
+
 	// and_reg_t2: 1110 1010 000x xxxx 0xxx xxxx xxxx xxxx
 	register_opcode_mask_32_ex(0xea000000, 0x15e08000, and_reg_t2,
 			0x00100f00, 0x0,
+			0, 0);
+
+	// bfc_t1: 1111 0011 0110 1111 0xxx xxxx xx0x xxxx
+	register_opcode_mask_32(0xf36f0000, 0x0c908020, bfc_t1);
+
+	// bfi_t1: 1111 0011 0110 xxxx 0xxx xxxx xx0x xxxx
+	register_opcode_mask_32_ex(0xf3600000, 0x0c908020, bfi_t1,
+			0xf0000, 0x0,
 			0, 0);
 
 	// bic: 0100 0011 10<x's>
