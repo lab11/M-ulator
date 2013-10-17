687e050c112693cba206bc80882fc3eaad06d6b0
Pat Pannuto <pat.pannuto@gmail.com>
2013-10-17 00:26:59 -0400

    Update dma_test for mmio endianness
    Tools print bytes sequentially, update the dma_test program to
    send 'deadbeef' such that it prints sanely

--- platforms/m3_ctl/software/dma_test/dma_test.c
+++ platforms/m3_ctl/software/dma_test/dma_test.c
@@ -9,7 +9,10 @@ void delay(unsigned ticks) {
 
 int main() {
 	while (1) {
-		write_i2c_message(0x98, 4, 0xdeadbeef);
-		delay(10);
+		// M3 MMIO i2c interface sends from least sig byte
+		// to most sig byte, but display tools all print one
+		// byte at a time; set this up so deadbeef prints correctly
+		write_i2c_message(0x98, 4, 0xefbeadde);
+		delay(1000);
 	}
 }
