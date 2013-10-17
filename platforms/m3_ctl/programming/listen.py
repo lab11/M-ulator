2aad901763481233df577dd163d34a20c690ce10
Pat Pannuto <pat.pannuto@gmail.com>
2013-10-17 12:06:43 -0400

    Use consistent logging interface

--- platforms/m3_ctl/programming/listen.py
+++ platforms/m3_ctl/programming/listen.py
@@ -6,27 +6,28 @@ import sys
 import os
 import Queue
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger('program')
 
-logging.info("-" * 80)
-logging.info("-- M3 Programmer")
-logging.info("")
+logger.info("-" * 80)
+logger.info("-- M3 Programmer")
+logger.info("")
 
 from ice import ICE
 ice = ICE()
 
 if len(sys.argv) not in (2,):
-    logging.info("USAGE: %s SERIAL_DEVICE\n" % (sys.argv[0]))
+    logger.info("USAGE: %s SERIAL_DEVICE\n" % (sys.argv[0]))
     sys.exit(2)
 
 # Callback for async I2C message
 def async_helper(msg_type, event_id, length, msg):
-    logging.debug("Bin Helper got msg len" + str(len(msg)))
+    logger.debug("Bin Helper got msg len" + str(len(msg)))
     if len(msg) == 0:
-        logging.debug("Ignore msg of len 0")
+        logger.debug("Ignore msg of len 0")
         return
     #msq_q.put(msg)
-    logging.info("Got msg: " + msg.encode('hex'))
+    logger.info("Got msg: " + msg.encode('hex'))
 
 #msq_q = Queue.Queue()
 ice.msg_handler['d+'] = async_helper
@@ -38,8 +39,8 @@ resp = raw_input("Would you like to send the DMA start interrupt? [Y/n] ")
 if len(resp) != 0 and resp[0] in ('n', 'N'):
     sys.exit()
 
-logging.info("Sending 0x88 0x00000000")
+logger.info("Sending 0x88 0x00000000")
 ice.i2c_send(0x88, "00000000".decode('hex'))
 
-logging.info("Listening...")
+logger.info("Listening...")
 resp = raw_input("Press enter at any time to quit\n")
