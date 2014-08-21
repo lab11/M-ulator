430d21163fb16de23cf1249b1496d801a18f243e
M3 <M3@VLSILAB-06.(none)>
2014-08-21 14:23:38 -0400

    adding sleep FFR code, upudating FFR code

--- platforms/m3/prc_v8/software/include/FFRv1.h
+++ platforms/m3/prc_v8/software/include/FFRv1.h
@@ -6,12 +6,12 @@
 //Register 0
 typedef union ffrv1_r0{
   struct{
-    unsigned TX_pulse_b	: 4;
     unsigned TX_VCO_b 	: 7;
+    unsigned TX_pulse_b	: 4;
   };
   uint32_t as_int;
 } ffrv1_r0_t;
-#define FFRv1_R0_DEFAULT {{0x4, 0x18}}
+#define FFRv1_R0_DEFAULT {{0x18, 0x4}}
 _Static_assert(sizeof(ffrv1_r0_t) == 4, "Punned Structure Size");
 
 //Register 1
@@ -28,39 +28,39 @@ _Static_assert(sizeof(ffrv1_r1_t) == 4, "Punned Structure Size");
 //TX_FSM_FSM_STATE is Read Only
 typedef union ffrv1_r2{
   struct{
-    unsigned TX_FSM_FSM_STATE	: 3;
-    unsigned TX_FSM_T_Z_DEL	: 8;
-    unsigned TX_FSM_N_HEADER	: 4;
-    unsigned TX_FSM_TX2TX	: 8;
     unsigned TX_FSM_START	: 1;
+    unsigned TX_FSM_TX2TX	: 8;
+    unsigned TX_FSM_N_HEADER	: 4;
+    unsigned TX_FSM_T_Z_DEL	: 8;
+    unsigned TX_FSM_FSM_STATE	: 3;
   };
   uint32_t as_int;
 } ffrv1_r2_t;
-#define FFRv1_R2_DEFAULT {{0x3, 0x10, 0x4, 0x20, 0x1}}
+#define FFRv1_R2_DEFAULT {{0x0, 0x20, 0x4, 0x10, 0x0}}
 _Static_assert(sizeof(ffrv1_r2_t) == 4, "Punned Structure Size");
 
 //Register 3
 typedef union ffrv1_r3{
   struct{
-    unsigned I_LIMIT_IL_cal32_0	: 5; 
-    unsigned I_LIMIT_IL_bypass	: 4;
-    unsigned I_LIMIT_IL_iTune	: 6;
     unsigned I_LIMIT_IL_EN	: 1;
+    unsigned I_LIMIT_IL_iTune	: 6;
+    unsigned I_LIMIT_IL_bypass	: 4;
+    unsigned I_LIMIT_IL_cal32_0	: 5; 
   };
   uint32_t as_int;
 } ffrv1_r3_t;
-#define FFRv1_R3_DEFAULT {{0x1F, 0xF, 0x0, 0x0}}
+#define FFRv1_R3_DEFAULT {{0x0, 0x0, 0xF, 0x1F}}
 _Static_assert(sizeof(ffrv1_r3_t) == 4, "Punned Structure Size");
 
 //Register 4
 typedef union ffrv1_r4{
   struct{
-    unsigned CLKGEN_RINGSEL	: 4;
-    unsigned I_LIMIT_IL_cal16 	: 8;
-    unsigned I_LIMIT_IL_cal32_2	: 5;
     unsigned I_LIMIT_IL_cal32_1	: 5;
+    unsigned I_LIMIT_IL_cal32_2	: 5;
+    unsigned I_LIMIT_IL_cal16 	: 8;
+    unsigned CLKGEN_RINGSEL	: 4;
   };
   uint32_t as_int;
 } ffrv1_r4_t;
-#define FFRv1_R4_DEFAULT {{0xFF, 0x1F, 0x1F, 0x0}}
+#define FFRv1_R4_DEFAULT {{0x1F, 0x1F, 0xFF, 0x0}}
 _Static_assert(sizeof(ffrv1_r4_t) == 4, "Punned Structure Size");
