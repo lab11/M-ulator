6d598a50bf44f29b73d799041e84c06b2db7e1cd
U-VLSILAB-21\M3 <githubm3@github.com>
2015-10-06 20:52:24 -0400

    1.9

--- platforms/m3/prc_v11/software/Pstack_ondemand_v1.9/Pstack_ondemand_v1.9.c
+++ platforms/m3/prc_v11/software/Pstack_ondemand_v1.9/Pstack_ondemand_v1.9.c
@@ -309,13 +309,13 @@ static void cdc_power_off(){
 
 inline static void set_pmu_sleep_clk_low(){
     // PRCv11 Default: 0x8F770049
-    //*((volatile uint32_t *) 0xA200000C) = 0x8F77183B; // 0x8F77003B: use GOC x0.6-2
-    *((volatile uint32_t *) 0xA200000C) = 0x8F77194B; // Sleep current 3nA
-    //*((volatile uint32_t *) 0xA200000C) = 0x8F7718CB; // Sleep current 5nA
+    //*((volatile uint32_t *) 0xA200000C) = 0x8F77183B; // 0x8F77003B: Sleep current 1.5nA, use GOC x0.6-2
+    *((volatile uint32_t *) 0xA200000C) = 0x8F77194B; // 0x8F77194B: Sleep current 3nA, use GOC x9-15
+    //*((volatile uint32_t *) 0xA200000C) = 0x8F7718CB; // 0x8F7718CB: Sleep current 5nA, use GOC x9-15
 }
 inline static void set_pmu_sleep_clk_default(){
     // PRCv11 Default: 0x8F770049
-    *((volatile uint32_t *) 0xA200000C) = 0x8F77184B; // 0x8F77184B: Sleep current 8nA, use GOC x9-18
+    *((volatile uint32_t *) 0xA200000C) = 0x8F77184B; // 0x8F77184B: Sleep current 8nA, use GOC x9-15
 }
 static void process_data(){
     uint8_t i;
