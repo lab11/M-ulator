86937b3af74e5d4e9d0e9376d7f480a68a61d6fa
U-VLSILAB-21\M3 <githubm3@github.com>
2016-10-07 11:35:04 -0400

    asdf

--- platforms/m3/prc_v13/software/MOD_motion_img_v6_PRCv13_PMUv2_FLPv2L_MDv4/MOD_motion_img_v6_PRCv13_PMUv2_FLPv2L_MDv4.c
+++ platforms/m3/prc_v13/software/MOD_motion_img_v6_PRCv13_PMUv2_FLPv2L_MDv4/MOD_motion_img_v6_PRCv13_PMUv2_FLPv2L_MDv4.c
@@ -1,8 +1,9 @@
 //****************************************************************************************************
 //Author:       Gyouho Kim
 //Description: 	MODv2.0 System Code
-//				Imaging and motion detection with MDv3
-//				FLASH storage with FLSv2
+//				Imaging and motion detection with MDv3 or MDv4
+//				FLASH storage with FLPv2
+//				PRCv13 & PRCv14
 //*******************************************************************
 #include "PRCv13.h"
 #include "PRCv13_RF.h"
