50df201be2cf5dc27a5400150c1b41798785474d
githubm3 <githubm3@github.com>
2018-02-05 15:54:44 -0500

    Added HTS221 header

---
+++ platforms/m3/pre_v14/software/include/HTS221.h
@@ -0,0 +1,30 @@
+//*******************************************************************
+//Author: ZhiYoong Foo
+//Description: HTS221 header file
+//*******************************************************************
+#define HTS221_WHO_AM_I       0x0F
+#define HTS221_AV_CONF        0x10
+#define HTS221_CTRL_REG1      0x20
+#define HTS221_CTRL_REG2      0x21
+#define HTS221_CTRL_REG3      0x22
+#define HTS221_STATUS_REG     0x27
+#define HTS221_HUMIDITY_OUT_L 0x28
+#define HTS221_HUMIDITY_OUT_H 0x29
+#define HTS221_TEMP_OUT_L     0x2A
+#define HTS221_TEMP_OUT_H     0x2B
+#define HTS221_CALIB_0        0x30
+#define HTS221_CALIB_1        0x31
+#define HTS221_CALIB_2        0x32
+#define HTS221_CALIB_3        0x33
+#define HTS221_CALIB_4        0x34
+#define HTS221_CALIB_5        0x35
+#define HTS221_CALIB_6        0x36
+#define HTS221_CALIB_7        0x37
+#define HTS221_CALIB_8        0x38
+#define HTS221_CALIB_9        0x39
+#define HTS221_CALIB_A        0x3A
+#define HTS221_CALIB_B        0x3B
+#define HTS221_CALIB_C        0x3C
+#define HTS221_CALIB_D        0x3D
+#define HTS221_CALIB_E        0x3E
+#define HTS221_CALIB_F        0x3F
