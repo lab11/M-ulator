d534de75daf5a21a9e6b6027d4f0a629121ae88e
U-VLSILAB-21\M3 <githubm3@github.com>
2018-06-26 09:45:55 -0400

    T3.7

--- platforms/m3/prc_v17/software/Tstack_ondemand_v3.7/Tstack_ondemand_v3.7.c
+++ platforms/m3/prc_v17/software/Tstack_ondemand_v3.7/Tstack_ondemand_v3.7.c
@@ -1248,8 +1248,10 @@ static void operation_snt_calibration_radio(uint32_t tuning_num){
 	exec_count_irq_sub = exec_count_irq >>4; // Divide by 16
 	
 	if (exec_count_irq_sub < tuning_num){
-
-		if (exec_count_irq & 0xF == 0xF){ // every 16 iterations
+		// Debug
+		mbus_write_message32(0xBB,exec_count_irq);
+		
+		if ((exec_count_irq & 0xF) == 0xF){ // every 16 iterations
 			sntv1_r0A.TMR_DIFF_CON++; // Default: 0x3FFB
 			mbus_remote_register_write(SNT_ADDR,0x0A,sntv1_r0A.as_int);
 		}
